f3459dbbbe22703e30904d934bd1a8500765d911
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-02-10 21:05:44 +0530

    added code to fetch records using unique id

--- resource_server.py
+++ resource_server.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask import send_file
 
+from database_helper import DB_Helper
+
 class ResourceServer:
     def __init__(self, name) -> None:
         self.resource_server = Flask(name)
         self.setup_routes()
 
     def setup_routes(self):
-        self.resource_server.route('/')(self.hello_world)
+        pass
 
-    def run(self, **kwargs):
-        self.resource_server.run(**kwargs)
+    def request_processor(self):
+        pass
+    
+    def request_listener(self):
+        pass
 
-    def hello_world(self):
-        return '<h4>Hello World!</h4>'
+
     
 
 
