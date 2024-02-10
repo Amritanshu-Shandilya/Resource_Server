10561fd20bc2de90b18f61986b4877bbc0c66907
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-02-10 21:43:39 +0530

    added code to display the homepage of the server

--- resource_server.py
+++ resource_server.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import send_file
+from flask import render_template
+from flask import request
 
 from database_helper import DB_Helper
 
@@ -9,15 +10,19 @@ class ResourceServer:
         self.setup_routes()
 
     def setup_routes(self):
-        pass
+        self.resource_server.route('/')(self.home_screen)
 
+    def run(self, **kwargs):
+        self.resource_server.run(**kwargs)
+
+    def home_screen(self):
+        return render_template('home_page.html')
+    
     def request_processor(self):
         pass
-    
+
     def request_listener(self):
         pass
-
-
     
 
 
