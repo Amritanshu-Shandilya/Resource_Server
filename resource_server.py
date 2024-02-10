3fe8fbbff33112e1f1146df871b2e3673ceaed9d
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-02-11 02:00:43 +0530

    wrote code to send file contents as  plain text

--- resource_server.py
+++ resource_server.py
@@ -6,6 +6,11 @@ from database_helper import DB_Helper
 
 class ResourceServer:
     def __init__(self, name) -> None:
+
+        self.db_helper = DB_Helper()
+
+        self.extracted_data = None
+
         self.resource_server = Flask(name)
         self.setup_routes()
 
@@ -19,14 +24,20 @@ class ResourceServer:
     def home_screen(self):
         return render_template('home_page.html')
     
-    def request_processor(self, path):
-        pass
+    def request_processor(self):
+        id, name, lv1, lv2, lv3 = self.extracted_data
+        # creating the path
+        path = '/'+lv1+'/'+lv2+'/'+lv3+'/'+name+'.txt'
+
+        with open(path, 'r') as file:
+            file_content = file.read()
+        
+        return file_content
 
     def request_listener(self, unique_id):
-        db_helper = DB_Helper()
-        data = db_helper.fetch_record(unique_id=unique_id)
-        print(data)
-        return '<p>Displaying the data</p>'
+        self.extracted_data = self.db_helper.fetch_record(unique_id=unique_id)[0]
+        
+        self.request_processor()
 
         
 
