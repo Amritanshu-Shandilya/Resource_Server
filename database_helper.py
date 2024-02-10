66d95625ae8cc7effd5d5aad61c806c10a12288e
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-02-10 20:35:19 +0530

    added functions to insert records into the table

--- database_helper.py
+++ database_helper.py
@@ -5,3 +5,16 @@ class DB_Helper:
     def __init__(self) -> None:
         self.con = sqlite3.connect('file_database.db')
         self.cur = self.con.cursor()
+
+
+    def add_file_record(self, tuple):
+        ''' Inserts new data into the files table. '''
+        query = "INSERT INTO files VALUES(?, ?, ?, ?)"
+        self.cur.execute(query, tuple)
+        self.con.commit()
+        return True
+    
+
+    def add_to_history(self, data):
+        ''' Inserts data into the history table. '''
+        pass
