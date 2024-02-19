f96d0ae0ab6120ece9020f0662b671bfa3155876
Amritanshu-Shandilya <sandyamritanshu9715@gmail.com>
2024-02-19 16:05:53 +0530

    added data insto the database

--- database_creator.py
+++ database_creator.py
@@ -33,7 +33,35 @@ cur = conn.cursor()
 # );'''
 
 "Verify the creation of the tables"
-query = "SELECT name FROM sqlite_master WHERE type='table';"
-res = cur.execute(query)
-print(res.fetchall())
+# query = "SELECT name FROM sqlite_master WHERE type='table';"
+# res = cur.execute(query)
+# print(res.fetchall())
 # conn.commit()
+
+"Entering values inside files table"
+# query = '''
+# INSERT INTO files VALUES('20240209-A23bC87dE1','dandakosaurus','text','information','dianosaur','EXH-A-001'),
+#                         ('20240209-Xyz456Pqrs','indosaurus','text','information','dianosaur','EXH-A-002'),
+#                         ('20240209-Lmno789Abc','saurian_rellic_01','text','information','dianosaur','EXH-A-003'),
+#                         ('20240209-DfgHij012K','thar_desert_ovum','text','information','dianosaur','EXH-A-004'),
+#                         ('20240209-ZzxYyWwVvU','tharosaurus_indicus','text','information','dianosaur','EXH-A-005'),
+#                         ('20240209-Klm876NopQ','hamsa_damyanti_rrv','text','information','paintings','EXH-B-001'),
+#                         ('20240209-Jkl321XyZa','shakuntalam_rrv','text','information','paintings','EXH-B-002'),
+#                         ('20240209-PqR543lmno','the_milkmaid_rrv','text','information','paintings','EXH-B-003'),
+#                         ('20240209-AbC98dEfGh','there_comes_papa_rrv','text','information','paintings','EXH-B-004'),
+#                         ('20240209-UVWxyz12Ab','yashoda_krishna_rrv','text','information','paintings','EXH-B-005'),
+#                         ('20240209-XyZ67AbCde','balochistan_pot','text','information','history','EXH-C-001'),
+#                         ('20240209-Lkji21NhgF','dancing_girl_harappa','text','information','history','EXH-C-002'),
+#                         ('20240209-GHTre098Fg','deedarganj_yakshini','text','information','history','EXH-C-003'),
+#                         ('20240209-567BcdEfgH','samudragupta_coin','text','information','history','EXH-C-004'),
+#                         ('20240209-Pqrs90AaBc','sarnath_pillar','text','information','history','EXH-C-005')'''
+
+"Entering values inside users table"
+# query = '''
+# INSERT INTO users VALUES('amrit_01', 'Amritanshu'),
+#                         ('rocky_05', 'Deepak'),
+#                         ('nobi_003', 'Sameer')'''
+
+
+
+conn.close()
